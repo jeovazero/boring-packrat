3b57217d4ea8e5a31037cd2c30bbb238a211db05
jeovazero <contato@jeova.ninja>
2021-06-10 23:59:23 -0300

    Add initial nix shell

---
+++ shell.nix
@@ -0,0 +1,24 @@
+let
+
+  nixpkgs = import ./nix/pinnedNix.nix { };
+
+  inherit (nixpkgs) pkgs;
+
+  inherit (pkgs)
+    haskell cabal2nix cabal-install;
+
+  haskellPackages = haskell.packages.ghc884;
+
+  # hls doesn't work well with haskell templates
+  # https://github.com/haskell/haskell-language-server/issues/1431
+  ghcide = haskellPackages.ghcide;
+
+in pkgs.stdenv.mkDerivation {
+  name = "shell";
+
+  buildInputs = [
+    cabal-install
+    cabal2nix
+    ghcide
+  ];
+}
