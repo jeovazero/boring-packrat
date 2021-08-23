c04bba4cef087db838a2cd6e2956b6d1256f2375
jeovazero <contato@jeova.ninja>
2021-08-23 00:49:40 -0300

    feat(boring-packrat): add the pretty print

--- lib/BoringPackrat.hs
+++ lib/BoringPackrat.hs
@@ -9,7 +9,8 @@ module BoringPackrat (
   ParsedResult(..),
   Result(..),
   Layer(..),
-  substr
+  substr,
+  prettyPrint
 ) where
 
 import Data.ByteString as B
@@ -122,6 +123,49 @@ data AST
   | Str ByteString
   | Void
   deriving (Show)
+
+applyIdent ident = B8.putStr $ B8.replicate ident ' '
+
+loopList input prefix ident (ast:xs) = do
+  applyIdent ident
+  B8.putStr prefix
+  prettyPrint' (ident + 2) True input ast
+  loopList input prefix ident xs
+loopList _ _ _ [] = pure ()
+
+prettyPrint = prettyPrint' 0 False
+
+prettyPrint' ident ignoreIdent input ast =
+  let
+    applyIdent' = if ignoreIdent then pure () else applyIdent ident
+    padding = if ignoreIdent then 0 else 2
+    showInfo range = do
+      P.putStr $ show range ++ " -> "
+      P.putStr "\""
+      B8.putStr $ substr range input
+      P.putStrLn "\""
+  in
+  case ast of
+    Cons' range ast1 ast2 -> do
+      applyIdent'
+      P.putStr "Cons -> "
+      showInfo range
+      loopList input ":" (ident + padding) [ast1,ast2]
+    Seq range asts -> do
+      applyIdent'
+      P.putStr "Sequence -> "
+      showInfo range
+      loopList input "-" (ident + padding) asts
+    Rule range name ast -> do
+      applyIdent'
+      B8.putStr $ B8.concat ["Rule ",name," -> "]
+      showInfo range
+      prettyPrint' (ident + padding) False input ast
+    Str value -> do
+      applyIdent'
+      P.putStrLn ("Str " ++ show value)
+    Void -> do
+      P.putStrLn "*"
 
 isVoid Void = True
 isVoid _ = False
