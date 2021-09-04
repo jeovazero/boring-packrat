68229e17d290acaf5f2e409a45c647d5fabbd0b8
jeovazero <contato@jeova.ninja>
2021-09-04 06:13:03 -0300

    docs: add example

---
+++ Example.hs
@@ -0,0 +1,58 @@
+{-# LANGUAGE OverloadedStrings #-}
+import BoringPackrat (
+    parse,
+    astFrom,
+    AST(..),
+    Terminal'(..),
+    PEG(..),
+    Grammar
+  )
+import BoringPackrat.PrettyPrint (prettyPrint)
+import qualified Data.Word8 as W
+import qualified Data.ByteString.Char8 as B8
+import Data.Maybe (fromJust)
+
+n = NonTerminal
+_ParenLeft  = Terminal $ Lit W._parenleft  -- '('
+_ParenRight = Terminal $ Lit W._parenright -- ')'
+_Times      = Terminal $ Lit W._asterisk   -- '*'
+_Plus       = Terminal $ Lit W._plus       -- '+'
+
+grammar :: Grammar
+grammar =
+  [ ("Expr", Choice [n"Add",n"Addend"])
+  , ("Add", Sequence [n"Addend",_Plus,n"Expr"])
+  , ("Addend", Choice [n"Mult",n"Factor"])
+  , ("Mult", Sequence [n"Factor",_Times,n"Addend"])
+  , ("Factor", Choice [n"Parens",n"Decimal"])
+  , ("Parens", Sequence [_ParenLeft,n"Expr",_ParenRight])
+  , ("Decimal", Many1 $ Terminal Digit)
+  ]
+
+data Expr
+    = Add Expr Expr
+    | Mult Expr Expr
+    | Decimal Int
+    deriving (Show)
+
+transformAST :: AST -> Expr
+transformAST ast =
+  case ast of
+    Rule _ "Add" (Seq _ [a,_,b]) ->
+      Add (transformAST a) (transformAST b)
+    Rule _ "Mult" (Seq _ [a,_,b]) ->
+      Mult (transformAST a) (transformAST b)
+    Rule _ "Parens" (Seq _ [_,e,_]) ->
+      transformAST e
+    Rule _ "Decimal" (Str d) ->
+      Decimal (read $ B8.unpack d)
+    Rule _ _ e -> transformAST e
+    _ -> error "Not implemented"
+
+main = do
+  let input = "(1+2)*3"
+  let result = parse grammar "Expr" input
+  let ast = fromJust $ astFrom result
+
+  prettyPrint input ast
+  print $ transformAST ast
